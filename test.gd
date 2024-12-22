f3355317511d095d3da425705b9759d399ed39f4
Cesare Montresor <cesare.montresor@gmail.com>
2024-12-22 21:33:24 +0100

    - interactive

--- test.gd
+++ test.gd
@@ -9,11 +9,11 @@ func _ready():
 	print(Time.get_datetime_string_from_system())
 	llm_engine.model_path = "./models/Meta-Llama-3-8B-Instruct-Q5_K_M.gguf" ##Your model path
 	llm_engine.n_predict = 200
-	#llm_engine.instruct = true
+	llm_engine.instruct = false
 	llm_engine.should_output_prompt = false
 	llm_engine.should_output_special = false
 	llm_engine.context_size = 1024
-	
+	llm_engine.main_gpu = 1
 	llm_input.text = 'ciao, chi sei ?'
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,12 +27,17 @@ func _on_send_pressed() -> void:
 	var benno_init = DataLoader.benno_init
 	
 	#llm_engine.input_prefix = benno_init
-	
+	llm_output.text = ""
 	var prompt = benno_init + "\n\n" + llm_input.text
-	var generated_text = llm_engine.generate_text_simple( prompt )
-	llm_output.text = generated_text
+	llm_engine.run_generate_text(prompt,"","")
+	
 	
 	
 	print(Time.get_datetime_string_from_system())
 	
+	pass # Replace with function body.
+
+
+func _on_llm_engine_generate_text_updated(new_text: String) -> void:
+	llm_output.text += new_text
 	pass # Replace with function body.
