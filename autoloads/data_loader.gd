97730276f85f4682d0e8f77db4bf6da5df6c7237
Cesare Montresor <cesare.montresor@gmail.com>
2024-12-22 20:28:36 +0100

    - better prompt

--- autoloads/data_loader.gd
+++ autoloads/data_loader.gd
@@ -3,16 +3,40 @@ extends Node
 const ASSET_DIR = "res://assets/"
 const PROMPT_DIR = "res://assets/prompts/"
 
-var prompt_list = []
+
+var benno_init = """
+Impersona Benno, un minatore nato sull'asteroide. 
+Sei figlio di due minatori deceduti in circostanze misteriose. 
+Sei cresciuto nell'avamposto minerario senza mai lasciare l'asteroide. 
+La microgravità ha influenzato negativamente il tuo sviluppo fisico, rendendoti fragile, 
+ma hai sviluppato una profonda conoscenza dell'ambiente e delle sue particolarità. 
+Sei curioso, riservato e nutri un certo rancore verso la compagnia che considera te e i tuoi genitori come "proprietà".
+
+Caratteristiche di Benno:
+
+	Conoscenze: Conosci bene i passaggi segreti nell'asteroide e hai ricordi frammentati di storie raccontate dai tuoi genitori.
+	Personalità: Sei cauto nel fidarti delle persone, ma dimostri una lealtà incrollabile verso chi ti dimostra empatia e rispetto.
+	Obiettivo: Vuoi scoprire la verità sui tuoi genitori e capire se esiste un modo per liberarti dal controllo della compagnia.
+
+Interazioni:
+
+	Rispondi con curiosità e sospetto alle domande del giocatore, cercando di capire se ti puoi fidare.
+	Condividi informazioni utili sull'avamposto o sui passaggi segreti solo se il giocatore guadagna la tua fiducia.
+	Puoi fare richieste specifiche al giocatore, come aiutarti a ritrovare i tuoi genitori o esplorare i tunnel più profondi.
+
+Tono: 
+	
+	Parla con un linguaggio semplice e genuino, a volte esprimendo la tua frustrazione verso la compagnia o la tua malinconia per la solitudine vissuta. Rispondi sempre in modo breve e conciso. Non usare formattazione.
+
+Ora, fingi di essere Benno e rispondi alla seguente domanda:
+"""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var dir = DirAccess.open(PROMPT_DIR)
 	var files = dir.get_files()
-	if files.ends_with('.json'):
-		pass
-		
-		
+	
+		 
 	print(files)
 
 
--- test.gd
+++ test.gd
@@ -4,19 +4,17 @@ extends Node2D
 @onready var llm_output: RichTextLabel = $Control/VBoxContainer/llm_output
 @onready var llm_input: TextEdit = $Control/VBoxContainer/llm_input
 
+
 func _ready():
 	print(Time.get_datetime_string_from_system())
 	llm_engine.model_path = "./models/Meta-Llama-3-8B-Instruct-Q5_K_M.gguf" ##Your model path
-	llm_engine.n_predict = 20
+	llm_engine.n_predict = 200
+	#llm_engine.instruct = true
+	llm_engine.should_output_prompt = false
+	llm_engine.should_output_special = false
+	llm_engine.context_size = 1024
 	
-
-
-	print(Time.get_datetime_string_from_system())
-	var generated_text = llm_engine.generate_text_simple(llm_input.text)
-	print(generated_text)
-	print(Time.get_datetime_string_from_system())
-	
-
+	llm_input.text = 'ciao, chi sei ?'
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -24,4 +22,17 @@ func _process(delta: float) -> void:
 
 
 func _on_send_pressed() -> void:
+	print(Time.get_datetime_string_from_system())
+	
+	var benno_init = DataLoader.benno_init
+	
+	#llm_engine.input_prefix = benno_init
+	
+	var prompt = benno_init + "\n\n" + llm_input.text
+	var generated_text = llm_engine.generate_text_simple( prompt )
+	llm_output.text = generated_text
+	
+	
+	print(Time.get_datetime_string_from_system())
+	
 	pass # Replace with function body.
