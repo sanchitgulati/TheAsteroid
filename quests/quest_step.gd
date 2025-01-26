c47d43047c875d5b921971005ef172891211fbcb
Cesare Montresor <cesare.montresor@gmail.com>
2025-01-26 13:20:13 +0100

    - refactyoring

--- quests/quest_step.gd
+++ quests/quest_step.gd
@@ -2,7 +2,6 @@ extends Resource
 
 class_name quest_step
 
-@export var quest_uid = "quest_1"
 @export_multiline var description = "Description"
 @export var request_items: Array[item_data] = []
 @export var request_npcs: Array[npc_data] = []
