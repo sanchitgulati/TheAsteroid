30844b4bc2b7409e844ea1a26adfccf6195bf719
Cesare Montresor <cesare.montresor@gmail.com>
2025-01-26 12:25:30 +0100

    - fixing dialog

--- UI/dialogue.gd
+++ UI/dialogue.gd
@@ -17,7 +17,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	
+	if visible and Input.is_action_pressed("esc"):
+		visible = false
+	
 	WorldState.PLAYER.can_move = !visible
+	
 	pass
 	
 
