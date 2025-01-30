395387072fe270f6d06306b88dcc458636099a64
papalagi <registro@eperinelli.com>
2025-01-30 22:29:36 +0100

    Player Movement con fisica
    Collisions Pareti

--- elements/player/player.gd
+++ elements/player/player.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
-
 class_name Player
 # to add Animation check:
 # https://docs.godotengine.org/en/stable/getting_started/first_2d_game/03.coding_the_player.html
-@export var speed = 400 # How fast the player will move (pixels/sec).
+@export var speed : float = 300.0  # Movement speed
+@export var acceleration : float = 500.0  # Smooth acceleration
+@export var friction : float = 500.0  # Friction when stopping
+
+var direction := Vector2.ZERO
+
 var screen_size # Size of the game window.
 var can_move = true
 
@@ -14,25 +18,14 @@ func _ready():
 	#var screen_center = Vector2(screen_size.x/2, screen_size.y/2)
 	#position = screen_center
 
-func _process(delta):
-	if !can_move: return
-	
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+func get_input():
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_dir * speed
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		#$AnimatedSprite2D.play()
-	else:
-		pass
-		#$AnimatedSprite2D.stop()
-		
-	position += velocity * delta
-	#position = position.clamp(Vector2.ZERO, screen_size)
+func _physics_process(delta):
+	if !can_move: return
+	get_input()
+	move_and_collide(velocity * delta)
+
+	## Move and slide
+	#move_and_slide()
