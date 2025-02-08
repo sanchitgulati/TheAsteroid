d442f66a0022fe8135ba6e14178cbcf01f728da2
Cesare Montresor <cesare.montresor@gmail.com>
2025-02-08 11:31:20 +0100

    - doors

---
+++ elements/gates/door_data.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+class_name door_data
+
+@export var open_texture: Texture2D
+@export var close_texture: Texture2D
+@export var item_unlock: item_data
--- elements/items/item_data.gd
+++ elements/items/item_data.gd
@@ -7,3 +7,5 @@ class_name item_data
 @export var name: String = ""
 @export var description: String = ""
 @export var texture: Texture2D
+@export var quest_item: bool = false
+@export var achievement: quest_data
