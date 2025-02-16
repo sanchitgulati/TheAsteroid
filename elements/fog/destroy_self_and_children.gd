98cde182bfac31408f7cba7c85d3356adbd5a72b
papalagi <registro@eperinelli.com>
2025-02-16 23:09:33 +0100

    Anim Fog of war

--- elements/fog/destroy_self_and_children.gd
+++ elements/fog/destroy_self_and_children.gd
@@ -1,12 +1,17 @@
 extends Area2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation_player.animation_finished.connect(_on_animation_finished)
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == 'Player':
+		print("Fade Out")
+		animation_player.play("fade_out")
+		
+func _on_animation_finished():
 		print("Destroy Self and Children!")
 		queue_free()
