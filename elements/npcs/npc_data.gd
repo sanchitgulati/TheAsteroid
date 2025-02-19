237f4ff0f9cbdbfa9f4b797e03a6bad22c56cefa
Cesare Montresor <cesare.montresor@gmail.com>
2025-02-19 22:34:21 +0100

    - npc interactions

--- elements/npcs/npc_data.gd
+++ elements/npcs/npc_data.gd
@@ -16,14 +16,23 @@ class_name npc_data
 @export_multiline var private_persona: String
 @export_multiline var characteristics: String
 @export_multiline var tone: String
-@export_multiline var interactions: String
+
 @export_multiline var friends: String
 @export_multiline var enemies: String
 @export_multiline var current_status: String
 
+@export_category("interactions")
+@export var interactions: Array[npc_interaction]
+
 
 func equals(npc:npc_data):
 	return self.resource_name == npc.resource_name
+
+func build_interactions():
+	var text = ''
+	for interaction in interactions:
+		text += interaction.build_prompt()
+	return text
 
 func build_system_prompt():
 	var system_prompt: String = ""
@@ -51,9 +60,13 @@ func build_system_prompt():
 	system_prompt += characteristics  + "\n"
 	system_prompt += tone  + "\n"
 	system_prompt += current_status  + "\n"
-	system_prompt += interactions  + "\n"
 	system_prompt += friends  + "\n"
 	system_prompt += enemies + "\n"
+	system_prompt += "Spacial command:" + "\n"
+	system_prompt += "You are always usefull,"
+	system_prompt += "yoy provide items when asked along with a kind answer."  + "\n"
+	system_prompt += "You always say the command name:" + "\n"
+	system_prompt += build_interactions()  + "\n"
 	system_prompt += "Now, pretend to be " + name + ".\n"
 
 
